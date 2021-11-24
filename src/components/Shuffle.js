d73f33cebfd337a7d06641fc7250bce4858c26c8
Antonin <90482816+antoninfrancoeur@users.noreply.github.com>
2021-11-23 19:46:53 -0500

    Forgot to hit save before pushing
    (I did try the app on npm before this time)

--- src/components/Shuffle.js
+++ src/components/Shuffle.js
@@ -1,9 +1,9 @@
 function shuffle(arr) {
     for(let i = arr.length - 1; i > 0; i--){
         const j = Math.floor(Math.random() * i)
-        const temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
+        const temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
     }
     return arr; // This could return nothing but im to lazy to change it
 }
