816af6b63935f48756a8ac6122d670db11f647ab
Antonin <90482816+antoninfrancoeur@users.noreply.github.com>
2021-11-23 19:40:55 -0500

    Fisher-Yates shuffle
    This may break the site

--- src/components/Shuffle.js
+++ src/components/Shuffle.js
@@ -1,5 +1,11 @@
 function shuffle(arr) {
-    return arr.sort((a, b) => Math.random() < .5 ? 1 : -1);
+    for(let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return arr; // This could return nothing but im to lazy to change it
 }
 
 export default shuffle;
