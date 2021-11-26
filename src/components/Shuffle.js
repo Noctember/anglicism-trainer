b47bba1c3d6a68dc900a4420cc485203e6c98327
Antonin <90482816+antoninfrancoeur@users.noreply.github.com>
2021-11-26 15:54:47 -0500

    Fixed shuffle to remove all biases once and for all (extensively tested)
    Also fixed 3 small mistakes in the list

--- src/components/Shuffle.js
+++ src/components/Shuffle.js
@@ -1,5 +1,12 @@
 function shuffle(arr) {
-    return arr.sort((a, b) => 0.5 - Math.random());
+    //return arr.sort((a, b) => 0.5 - Math.random());
+    for(let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
 }
 
 export default shuffle;
